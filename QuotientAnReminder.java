b3f22cdf9e5990c7799b050285732bc207388ae3
Rahul kothawade <93417709+rahulkotawade123@users.noreply.github.com>
2022-05-19 16:40:48 +0530

    QuotientAnReminder

--- QuotientAnReminder.java
+++ QuotientAnReminder.java
@@ -5,6 +5,15 @@ public class QuotientAnReminder {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+        int dividend = 556, divisor = 9;
+  
+        int quotient = dividend / divisor;
+        int remainder = dividend % divisor;
+  
+        System.out.println("The Quotient is = " + quotient);
+        System.out.println("The Remainder is = " + remainder);
+    }
+}
 	}
 
 }

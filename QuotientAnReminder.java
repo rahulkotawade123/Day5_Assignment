050077d044d2b6195cc02eee2134f0e4ea7938bd
Rahul Kothawade <rahulkothawade99@gmail.com>
2022-05-19 16:34:21 +0530

    Swap two numbers

---
+++ QuotientAnReminder.java
@@ -0,0 +1,10 @@
+package Day_5_Assignment;
+
+public class QuotientAnReminder {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ SwapTwoNumbers.java
@@ -0,0 +1,20 @@
+package Day_5_Assignment;
+
+public class SwapTwoNumbers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String i = "20";
+		String j = "30";
+		System.out.println(i);
+		System.out.println(j);
+
+		
+		String temp;
+		temp = i;
+		i = j;
+		j = temp;
+	}
+
+}
